c63c0d6440b55d85ca1131cae572c0fb8cca3dfd
normanblade <franos.95@gmail.com>
2020-03-12 18:25:01 +0100

    lol-3

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,12 +1,62 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Room {
 
-    private int lenght;
-    private int height;
 
-    
+public class Room implements IRoom{
 
+    private class Row{
+        List<Place> places;
+
+        Row(int size){
+            places = new ArrayList<>(size);
+        }
+
+        void setPlace(int index, Place place){
+            places.add(index, place);
+        }
+    }
+
+
+    private int length = -1;
+    private int width = -1;
+
+    private List<Row> rows;
+
+
+    private void generateMatrix(){
+        rows = new ArrayList<>(length);
+        for (Row r : rows) {
+            r = new Row(width);
+        }
+    }
+
+
+    @Override
+    public void setLength(int l) {
+        // y
+        length = l;
+
+        if(width > 0)
+            generateMatrix();
+    }
+
+    @Override
+    public void setWidth(int w) {
+        // x
+        width = w;
+
+        if(length > 0)
+            generateMatrix();
+    }
+
+    @Override
+    public void setPlace(int x, int y, Place p) {
+        if (x<0 || x>length || y<0 || y>width)
+            return;
+
+        rows.get(y).setPlace(x, p);
+    }
 }
